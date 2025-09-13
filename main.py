e3ec8fe1c6f011aeeca3699f3f24873a41f5c976
lakshmixr-ops <lakshmi.x.r@gmail.com>
2025-09-13 12:11:09 -0400

    Function to read the csv file with birthdays and print whoever's birthday matches today's date

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import csv
+from datetime import datetime
 
-birthdays_file = open('Birthday_Calendar.csv', 'r')
-birthdays_reader = csv.reader(birthdays_file)
-print(type(birthdays_reader))
+def check_today_bday():
+    file_path="C:\\Users\\lakshmi.ramanujam\\PycharmProjects\\Slackbot\\Birthday_Calendar.csv"
+    birthdays_file = open(file_path, 'r')
+    birthdays_reader = csv.reader(birthdays_file)
 
-for row in birthdays_reader:
-    print(row[1])
-    print(type(row))
+    today_date = datetime.today()
+
+
+    for bday in birthdays_reader:
+        parts_of_bday = bday[1].split("-")
+        bday_month = int(parts_of_bday[0])
+        bday_day = int(parts_of_bday[1])
+
+        if today_date.day == bday_day and today_date.month == bday_month:
+            print("Today is someone's birthday!", bday[0])
+
+check_today_bday()
+
+
+
+
+
+
+
+
