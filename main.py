73d1d31063ab85a2a4fdd478472fd240f45cbdae
lakshmixr-ops <lakshmi.x.r@gmail.com>
2025-09-14 21:21:18 -0400

    Function to read the csv file with birthdays and print whoever's birthday matches today's date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import requests
 
 def check_today_bday():
     file_path="C:\\Users\\lakshmi.ramanujam\\PycharmProjects\\Slackbot\\Birthday_Calendar.csv"
@@ -15,7 +16,24 @@ def check_today_bday():
         bday_day = int(parts_of_bday[1])
 
         if today_date.day == bday_day and today_date.month == bday_month:
-            print("Today is someone's birthday!", bday[0])
+            #gif_url = "dia.giphy.com/media/l0HlOvJ7yaacpuSas/giphy.gifhttps://me"
+            api_key = "HSQRyqasid5WknhApmQmSMQ1vqf6MK79"  # <-- your unique key from Giphy
+            url = f"https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag=birthday&rating=g"
+
+            response = requests.get(url)
+            data = response.json()
+
+            gif_url = data["data"]["images"]["downsized"]["url"]
+
+            message = {
+            "text": "Happy Birthday" + " " + bday[0] + ".Have a wonderful day!",
+                "attachments": [
+                    {
+                        "image_url": gif_url
+                    }
+                ]
+        }
+            requests.post("https://hooks.slack.com/services/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", json=message)
 
 check_today_bday()
 
